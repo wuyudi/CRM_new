af79b4357ab2c7d04b4b1ebe6ab137a178e8ea8e
wuyudi <wuyudi119@163.com>
2020-11-30 18:12:26 +0800

    📝 update register and views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import LoginModel
 
@@ -9,9 +9,9 @@ def hello(request):
 
 def login_view(request):
     username = request.POST.get("username")
-    error_msg = ''
+    error_msg = ""
     if len(str(username)) < 6:
-        error_msg = 'Too short username\n用户名过短，请重新输入'
+        error_msg = "Too short username\n用户名过短，请重新输入"
     data = LoginModel.objects.filter(username=username)
     if data:
         for item in data:
@@ -26,16 +26,38 @@ def login_view(request):
     context = {
         "error_msg": error_msg,
     }
-    if request.method == 'POST':
+    if request.method == "POST":
         print(request.POST)
-    return render(request, 'login.html', context)
+    return render(request, "login.html", context)
 
 
 def register_view(request):
-    return render(request, 'register.html', {})
+    username = request.POST.get("username")
+    password = request.POST.get("pass")
+    confirm_password = request.POST.get("confirm_pass")
+    db_username = LoginModel.objects.filter(username=username)
+    if password != confirm_password:
+        error_msg = "两次输入密码不一致"
+        print("different password")
+    else:
+        redirect("./")
+        # if len(str(username)) < 6:
+        #     error_msg = "用户名过短"
+        #     print(error_msg)
+        # elif db_username:
+        #     error_msg = "用户已存在"
+        #     print(error_msg)
+        # else:
+        #     new_user=LoginModel.objects.create()
+        #     new_user.username=username
+        #     new_user.password=password
+        #     new_user.save()
+        #     redirect("../")
+    return render(request, "register.html", {})
 
 
 def main_view(request):
-    return render(request, 'main.html', {})
+    return render(request, "main.html", {})
+
 
 # Create your views here.

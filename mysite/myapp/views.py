5eab953070cbf3c9411070ba773907f20901a837
wuyudi <wuyudi119@163.com>
2020-12-04 10:51:39 +0800

    🎨 rewrite

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,29 +1,28 @@
-from django.shortcuts import render, redirect
-from .models import LoginModel, CartModel, ProductsModel
+from django.shortcuts import render
+from .models import CartModel, ProductsModel
+
+
+# 通过request.REQUEST.getlist取到list形式的提交结果
+def getresult(request):
+    check_box_list = request.REQUEST.getlist("check_box_list")
 
 
 def shopping_view(request):
-    username = request.session["username"]
-    cart_data = CartModel.objects.filter(username=username)
+    cart_data = CartModel.objects.filter()
     cart_products = [item.number for item in cart_data]
-    print(cart_products)
-
     add_number = request.POST.get("add_number")
-    print(add_number)
     if add_number is not None:
-        cart_info = CartModel.objects.filter(username=username, number=add_number)
+        cart_info = CartModel.objects.filter(number=add_number)
         if not cart_info:
             new_record = CartModel.objects.create()
-            new_record.username = username
             new_record.number = add_number
             new_record.save()
-            print("save success")
     product_data_all = ProductsModel.objects.filter()
     product = []
     for item in product_data_all:
         product.append([item.number, item.brand, item.name, item.type, item.price])
     print(product)
-    context = {"username": username, "index": index, "product": product}
+    context = {"product": product}
     return render(request, "shopping.html", context)
 
 

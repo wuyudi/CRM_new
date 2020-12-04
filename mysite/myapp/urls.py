8e7f436f5a3f987f70eb6969dccc32ec6229c863
wuyudi <wuyudi119@163.com>
2020-12-04 11:08:52 +0800

    ✨ begin my project

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_view),
     path("shopping/", views.shopping_view),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import CartModel, ProductsModel
-
+from django.http import HttpRequest
 
 # 通过request.REQUEST.getlist取到list形式的提交结果
-def getresult(request):
-    check_box_list = request.REQUEST.getlist("check_box_list")
+def getresult(request: HttpRequest):
+
+    print(check_box_list)
+    return
 
 
-def shopping_view(request):
+def shopping_view(request: HttpRequest):
+    check_box_list = request.POST.getlist("check_box_list")
+    print(check_box_list)
     cart_data = CartModel.objects.filter()
     cart_products = [item.number for item in cart_data]
     add_number = request.POST.get("add_number")
@@ -21,7 +25,6 @@ def shopping_view(request):
     product = []
     for item in product_data_all:
         product.append([item.number, item.brand, item.name, item.type, item.price])
-    print(product)
     context = {"product": product}
     return render(request, "shopping.html", context)
 
